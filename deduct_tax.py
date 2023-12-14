2fa55d5feb8e0825e56b44a18a9f655558028dc4
Simpraiden <burapasiriwatnarapat@gmail.com>
2023-12-14 13:44:00 +0700

    Update deduct_tax.py

--- deduct_tax.py
+++ deduct_tax.py
@@ -1,6 +1,9 @@
 #first 
-print("Hello welcome to tax calcu ver 2566")
-all_income=int(input("Please input your yearly income :"))
+print("Hello welcome to CRU SMART tax calculator ver 2566")
+all_income=int(input("Please input your monthly income :"))
+all_income*=12
+all_income+=int(input("Please input your bonus this year :"))
+print(all_income)
 
 #ลดหย่อนเมีย
 wife = int(input("Do you have lawful wife? If you have, press 1. If you don't have, press 0 :"))
